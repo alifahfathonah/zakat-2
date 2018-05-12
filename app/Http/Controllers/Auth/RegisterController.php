6aaf41fb5c26d87d32f1e8e42ff10e452099b8d7
sunthree74 <nurmuhlis74@yahoo.co.id>
2018-05-12 09:27:13 +0700

    memperbaiki fitur validasi registrasi

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required|string|max:150',
+            'email' => 'required|string|email|max:150|unique:users',
+            'username' => 'required|string|max:25|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
